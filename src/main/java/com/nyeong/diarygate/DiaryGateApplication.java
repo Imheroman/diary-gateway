5d1e83c191ef5bdaa6a1a27ae25f09c9e8794c71
nyeong <duddnd5808@gmail.com>
2024-05-10 23:36:38 +0900

    base setting commit

---
+++ src/main/java/com/nyeong/diarygate/DiaryGateApplication.java
@@ -0,0 +1,13 @@
+package com.nyeong.diarygate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryGateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaryGateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nyeong/diarygate/DiaryGateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyeong.diarygate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryGateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
